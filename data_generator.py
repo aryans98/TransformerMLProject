dea351922648ae8b7ab6958e66b5a3f8c42f8424
aryans98 <aryansindhav@gmail.com>
2023-10-01 05:39:15 +0530

    updated files

--- data_generator.py
+++ data_generator.py
@@ -1,33 +1,44 @@
 import pandas as pd
 import random
+import string
 
-# Function to generate simulated data for transformers
+# Function to generate random string for transformer names
+def generate_random_name():
+    return ''.join(random.choice(string.ascii_uppercase) for _ in range(8))
+
+# Function to generate transformer data
 def generate_transformer_data(num_transformers):
     transformer_data = []
-    transformer_names = ["Transformer A", "Transformer B", "Transformer C", "Transformer D", "Transformer E"]
 
     for _ in range(num_transformers):
-        transformer = {
-            "Transformer Name": random.choice(transformer_names),
-            "Voltage Rating (kV)": round(random.uniform(10, 50), 2),
-            "Current Rating (A)": round(random.uniform(50, 500), 2),
-            "Power Rating (kVA)": round(random.uniform(50, 1000), 2),
-            "Efficiency (%)": round(random.uniform(90, 98), 2),
-            "Temperature (°C)": round(random.uniform(25, 90), 2),
-            "Weight (kg)": round(random.uniform(500, 5000), 2),
-            "Cost ($)": round(random.uniform(1000, 50000), 2),
-            "Noise Level (dB)": round(random.uniform(50, 80), 2),
-            "Reliability (years)": round(random.uniform(10, 30), 2)
-        }
-        transformer_data.append(transformer)
+        transformer_name = generate_random_name()
+        voltage_rating = random.uniform(5, 50)
+        current_rating = random.uniform(10, 200)
+        power_rating = voltage_rating * current_rating
+        efficiency = random.uniform(80, 99)
+        temperature = random.uniform(25, 90)
+        weight = random.uniform(100, 2000)
+        cost = random.uniform(1000, 5000)
+        noise_level = random.uniform(50, 80)
+        reliability = random.uniform(5, 30)
 
-    # Create a DataFrame from the generated data
-    df = pd.DataFrame(transformer_data)
-    return df
+        transformer_data.append([transformer_name, voltage_rating, current_rating, power_rating, efficiency,
+                                 temperature, weight, cost, noise_level, reliability])
 
-# Main function to generate data and save it to a CSV file
+    columns = ['Transformer Name', 'Voltage Rating (kV)', 'Current Rating (A)', 'Power Rating (kVA)',
+               'Efficiency (%)', 'Temperature (°C)', 'Weight (kg)', 'Cost ($)', 'Noise Level (dB)',
+               'Reliability (years)']
+
+    data_frame = pd.DataFrame(transformer_data, columns=columns)
+    return data_frame
+
+# Main function to handle user input and generate data
 if __name__ == "__main__":
-    num_transformers = int(input("Enter the number of transformers to generate data for: "))
-    data = generate_transformer_data(num_transformers)
-    data.to_csv("data/transformer_data.csv", index=False)
-    print(f"Simulated data for {num_transformers} transformers has been generated and saved to 'data/transformer_data.csv'.")
+    num_transformers = int(input("Enter the number of transformers to generate: "))
+    
+    if num_transformers <= 0:
+        print("Invalid input. Please enter a positive number of transformers.")
+    else:
+        transformer_data = generate_transformer_data(num_transformers)
+        transformer_data.to_csv('data/transformer_data.csv', index=False)
+        print(f'{num_transformers} transformers data has been generated and saved to "data/transformer_data.csv".')
